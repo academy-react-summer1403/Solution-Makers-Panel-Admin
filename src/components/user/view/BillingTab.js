60f96223ebab7bc356550904d20fabbd5f4a4045
saeed <plushyterrorblade@gmail.com>
2024-11-07 21:46:02 +0330

    added create new group , edit group , delete group logic

--- src/components/user/view/BillingTab.js
+++ src/components/user/view/BillingTab.js
@@ -6,17 +6,61 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
+  Form,
+  Input,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
   UncontrolledDropdown,
 } from "reactstrap";
 import instance from "../../../services/middleware";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { CheckCircle, MoreHorizontal, Trash } from "react-feather";
+import { useEffect, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Edit2, MoreHorizontal, Trash } from "react-feather";
 import { showApplyChangesSwal } from "../../../utility/Utils";
 import toast from "react-hot-toast";
 
+const schema = yup
+  .object({
+    groupName: yup
+      .string()
+      .required("نام گروه را وارد کنید")
+      .min(2, "حداقل 2 حرف")
+      .max(20, "حداکثر 20 حرف"),
+    groupCapacity: yup
+      .number()
+      .transform((value) => (Number.isNaN(value) ? null : value))
+      .nullable()
+      .required("ظرفیت گروه را وارد کنید")
+      .min(1, "حداقل 1 نفر")
+      .max(254, "حداکثر 254 نفر"),
+  })
+  .required();
+
 const BillingTab = () => {
   const { courseId } = useParams();
+  const [formModal, setFormModal] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    setValue,
+    getValues,
+    formState: { errors, isValid, isSubmitSuccessful },
+  } = useForm({
+    mode: "onTouched",
+    resolver: yupResolver(schema),
+    defaultValues: {
+      groupName: "",
+      groupCapacity: "",
+      groupId: "",
+    },
+  });
 
   const queryClient = useQueryClient();
 
@@ -33,13 +77,10 @@ const BillingTab = () => {
     });
   };
 
-  const createNewCourseGroup = () => {
-    const formData = new FormData();
-    formData.append("GroupName", "test group");
-    formData.append("CourseId", courseId);
-    formData.append("GroupCapacity", 50);
-    return instance.post("/CourseGroup", formData);
-  };
+  const createNewCourseGroup = (formData) =>
+    instance.post("/CourseGroup", formData);
+
+  const editCourseGroup = (formData) => instance.put("/CourseGroup", formData);
 
   const courseDetails = queryClient.getQueryData(["courseDetails", courseId]);
 
@@ -51,17 +92,20 @@ const BillingTab = () => {
 
   const { mutateAsync: deleteCourseGroupMutate } = useMutation({
     mutationFn: deleteCourseGroup,
-    onSuccess: () => {
-      refetch().then(() => toast.success("گروه حذف شد"));
-    },
+    onSuccess: () => refetch().then(() => toast.success("گروه حذف شد")),
     onError: (err) => toast.error(err.response.data.ErrorMessage[0]),
   });
 
   const { mutateAsync: addNewGroupMutate } = useMutation({
     mutationFn: createNewCourseGroup,
-    onSuccess: () => {
-      refetch().then(() => toast.success("گروه جدید ساخته شد"));
-    },
+    onSuccess: () => refetch().then(() => toast.success("گروه جدید ساخته شد")),
+    onError: (err) => toast.error(err.response.data.ErrorMessage[0]),
+  });
+
+  const { mutateAsync: editCourseGroupMutate } = useMutation({
+    mutationFn: editCourseGroup,
+    onSuccess: () => refetch().then(() => toast.success("گروه ویرایش شد")),
+    onError: (err) => toast.error(err.response.data.ErrorMessage[0]),
   });
 
   useEffect(() => {
@@ -70,7 +114,12 @@ const BillingTab = () => {
     }
   }, [courseDetails]);
 
-  console.log(data?.data);
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+      setFormModal(!formModal);
+    }
+  }, [isSubmitSuccessful]);
 
   const columns = [
     {
@@ -99,22 +148,23 @@ const BillingTab = () => {
                 tag="span"
                 className="w-100"
                 onClick={() => {
-                  // setStudentId(row.studentId);
-                  // refetch().then(() => setBasicModal(!basicModal));
+                  console.log(row);
+                  setValue("groupName", row.groupName);
+                  setValue("groupCapacity", row.groupCapacity);
+                  setValue("groupId", row.groupId);
+                  setFormModal(!formModal);
                 }}
               >
-                <CheckCircle size={14} className="me-50" />
+                <Edit2 size={14} className="me-50" />
                 <span className="align-middle">ویرایش</span>
               </DropdownItem>
               <DropdownItem
                 tag="span"
                 className="w-100"
-                onClick={
-                  () =>
-                    showApplyChangesSwal(() =>
-                      deleteCourseGroupMutate(row.groupId)
-                    )
-                  // showApplyChangesSwal(() => deletionMutate(row.reserveId))
+                onClick={() =>
+                  showApplyChangesSwal(() =>
+                    deleteCourseGroupMutate(row.groupId)
+                  )
                 }
               >
                 <Trash size={14} className="me-50" />
@@ -134,9 +184,79 @@ const BillingTab = () => {
   return (
     <>
       <div className="d-flex justify-content-end py-1">
-        <Button color="primary" onClick={() => addNewGroupMutate()}>
+        <Button
+          color="primary"
+          onClick={() => {
+            reset();
+            setFormModal(!formModal);
+          }}
+        >
           ساخت گروه
         </Button>
+        <Modal
+          isOpen={formModal}
+          toggle={() => setFormModal(!formModal)}
+          className="modal-dialog-centered"
+        >
+          <ModalHeader toggle={() => setFormModal(!formModal)}>
+            <span style={{ fontSize: 20 }}>
+              لطفا اطلاعات گروه را وارد نمایید
+            </span>
+          </ModalHeader>
+          <ModalBody>
+            <Form
+              onSubmit={handleSubmit((data) => {
+                if (getValues("groupId")) {
+                  const formData = new FormData();
+                  formData.append("Id", getValues("groupId"));
+                  formData.append("GroupName", data.groupName);
+                  formData.append("CourseId", courseId);
+                  formData.append("GroupCapacity", data.groupCapacity);
+                  editCourseGroupMutate(formData);
+                } else {
+                  const formData = new FormData();
+                  formData.append("GroupName", data.groupName);
+                  formData.append("CourseId", courseId);
+                  formData.append("GroupCapacity", data.groupCapacity);
+                  addNewGroupMutate(formData);
+                }
+              })}
+            >
+              <div className="mb-2">
+                <Controller
+                  name="groupName"
+                  control={control}
+                  render={({ field }) => (
+                    <Input {...field} placeholder="نام گروه" />
+                  )}
+                />
+                <p style={{ color: "red", marginTop: 5 }}>
+                  {errors.groupName?.message}
+                </p>
+              </div>
+              <div className="mb-2">
+                <Controller
+                  name="groupCapacity"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      type="number"
+                      {...field}
+                      placeholder="ظرفیت گروه (حداکثر 254 نفر)"
+                    />
+                  )}
+                />
+                <p style={{ color: "red", marginTop: 5 }}>
+                  {errors.groupCapacity?.message}
+                </p>
+              </div>
+              <Button color="primary" type="submit">
+                {!getValues("groupId") ? "افزودن گروه" : "ثبت تغییرات"}
+              </Button>
+            </Form>
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </Modal>
       </div>
       <Card className="overflow-hidden">
         <div className="react-dataTable">
